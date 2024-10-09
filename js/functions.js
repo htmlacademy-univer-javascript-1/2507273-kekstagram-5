0db3918906380eb60d31c2c79698fa5f26756bfe
Darya <minoginadara@gmail.com>
2024-10-09 16:36:47 +0500

    Добавит функции

---
+++ js/functions.js
@@ -0,0 +1,21 @@
+function checkLength(checkingStr, maxLength){
+  return checkingStr.length <= maxLength
+}
+
+function isPalindrom(checkingStr){
+    const cleanedStr = checkingStr.toLowerCase().replace(/ /g, '');
+    return cleanedStr === cleanedStr.split('').reverse().join('');
+}
+
+function chooseNumber(str) {
+  const checkingStr = str.toString();
+  let result = '';
+
+  for (let i = 0; i < checkingStr.length; i++) {
+      if (!isNaN(checkingStr[i]) && checkingStr[i] !== ' ') {
+          result += checkingStr[i];
+      }
+  }
+
+    return result.length ? parseInt(result, 10) : NaN;
+}
