697bc582e2d6b66db074da94713efef73cb802d6
Darya <minoginadara@gmail.com>
2024-10-27 23:15:15 +0500

    Добавит функцию проверки встреч

--- js/functions.js
+++ js/functions.js
@@ -20,6 +20,21 @@ function chooseNumber(str) {
   return result.length ? parseInt(result, 10) : NaN;
 }
 
+function isMeetingWithinWorkHours(startWork, endWork, startMeeting, durationMinutes) {
+  function timeToMinutes(timeStr) {
+    const [hours, minutes] = timeStr.split(':').map(Number);
+    return hours * 60 + minutes;
+  }
+
+  const startWorkMinutes = timeToMinutes(startWork);
+  const endWorkMinutes = timeToMinutes(endWork);
+  const startMeetingMinutes = timeToMinutes(startMeeting);
+  const endMeetingMinutes = startMeetingMinutes + durationMinutes;
+
+  return startWorkMinutes <= startMeetingMinutes && endMeetingMinutes <= endWorkMinutes;
+}
+
 checkLength('3dfggf345', 15);
 isPalindrom('123321');
 chooseNumber('13bvn34gcvbng34');
+isMeetingWithinWorkHours('08:00', '17:30', '14:00', 90);
