054836ae54f099442379754a94e568a783bb02e0
Darya <minoginadara@gmail.com>
2024-11-04 23:00:04 +0500

    Добавит модуль с отрисовкой миниатюр

---
+++ js/rendering.js
@@ -0,0 +1,23 @@
+import { generatePhotos } from './photo.js';
+
+const PICTURES_TITLE = document.querySelector('.pictures__title');
+PICTURES_TITLE.classList.remove('visually-hidden');
+
+const PICTURE_ELEMENT = document.querySelector('.pictures');
+const PICTURE_TEMPLATE = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const RANDOM_PICTURES = generatePhotos();
+const PICTURES_FRAGMENT = document.createDocumentFragment();
+
+RANDOM_PICTURES.forEach(({url, description, likes, comments}) => {
+  const PICTURE = PICTURE_TEMPLATE.cloneNode(true);
+  PICTURE.querySelector('.picture__img').src = url;
+  PICTURE.querySelector('.picture__img').alt = description;
+  PICTURE.querySelector('.picture__likes').textContent = likes;
+  PICTURE.querySelector('.picture__comments').textContent = comments;
+  PICTURES_FRAGMENT.appendChild(PICTURE);
+});
+
+PICTURE_ELEMENT.appendChild(PICTURES_FRAGMENT);
